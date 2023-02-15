341aaec612563d21a15dc7e35be7c595e97376af
Frankline Misango Oyolo <95913228+FranklineMisango@users.noreply.github.com>
2023-02-15 11:40:14 +0800

    Passing callbacks - Mechanics problem

--- add.js
+++ add.js
@@ -1,5 +1,21 @@
-const Add = (a ,b) => {
-    return (a + b)
+// You drop your car off at the mechanic and you want the shop to 'call you back' when your car is fixed.
+let carIsBroken = false;
+
+const callCarOwner = () => console.log("Hello your car is done!");
+
+const fixCar = (carIsBroken, callCarOwner) => {
+  if (carIsBroken === true) {
+    console.log("we are fixing the car")
+    carIsBroken = false
+    callCarOwner();
+  }
+  else if (carIsBroken === false) {
+    console.log("Your car has no issues")
+  } else {
+    console.log("No car is available for fixing")
+  }
+
+  console.log(carIsBroken);
 }
 
-console.log(Add(5,6))
+fixCar(carIsBroken, callCarOwner);
